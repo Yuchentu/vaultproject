9c635ddd1e61d601409efb8025ac90794dcb2375
Eddie Tu <Yuchentu@Eddies-MBP-2.fios-router.home>
2019-05-24 15:30:12 -0400

    first

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/test', methods = ['GET'])
+def test():
+	return 'This is working!'
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='0.0.0.0')
